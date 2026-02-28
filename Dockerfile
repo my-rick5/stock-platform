afc71f38234b6b2cda98d424a96afd0dc8bee33a
Zachary Myrick <zach.myrick@fullmeasureed.com>
2026-02-28 13:23:56 -0500

    cleanup

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM apache/airflow:2.7.0
-
-USER airflow
-RUN pip install apache-airflow-providers-amazon apache-airflow-providers-postgres pytest pytest-mock
--- analytics/macros/ref_override.sql
+++
@@ -1,7 +0,0 @@
-{% macro ref(model_name) %}
-   {% do return(builtins.ref(model_name).include(database=false, schema=false)) %}
-{% endmacro %}
-
-{% macro source(source_name, table_name) %}
-   {% do return(builtins.source(source_name, table_name).include(database=false, schema=false)) %}
-{% endmacro %}
